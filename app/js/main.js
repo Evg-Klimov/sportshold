89426939534c257fe673b67c2fc6bf80cec85e2c
Evgeny Klimov <klimov627273@mail.ru>
2020-04-09 16:15:53 +0400

    добавлена анимация для гамбургера

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
-const hamburger = document.querySelector(".hamburger");
-const mainMenu = document.querySelector(".main-menu");
+let hamburger = document.querySelector(".hamburger");
+let mainMenu = document.querySelector(".main-menu");
+let lins = document.querySelectorAll(".line");
 
 hamburger.addEventListener("click", () => {
 	mainMenu.classList.toggle("open");
+	for (let i = 1; i < lins.length; i++) {
+		lins[i].classList.toggle("full");
+	}
 });
+
